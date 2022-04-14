6adf43229a51eb95784cde920ce3bbb821afbdd0
dbtop <berdaddario@gmail.com>
2022-04-14 19:03:15 -0300

    primer commit

---
+++ src/pages/Inicio.jsx
@@ -0,0 +1,11 @@
+
+const Inicio = () => {
+      
+      return (
+            <div>
+                  <h1 className ="text-center font-bold text-3xl m-4">Inicio</h1>  
+            </div>
+      )
+}
+
+export default Inicio
---
+++ src/pages/NuevoCliente.jsx
@@ -0,0 +1,12 @@
+    import React from 'react'
+    
+    const NuevoCliente = () => {
+        return (
+              <div>   
+                  <h1>Nuevo Cliente</h1>
+              </div>
+        )
+    }
+    
+    export default NuevoCliente
+    
